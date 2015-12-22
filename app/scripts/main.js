169c6fe35febf957e37192db2cbad61c593631a4
Dan Kass <romeboards@gmail.com>
2015-12-22 12:50:01 -0500

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  var $hero = $('.hero');
+  var $heroContent = $('.hero-content');
+  var $header = $('header');
+  var faSVG = $('.fa-logo svg')[0];
+
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= 40){  $header.addClass('scrolled');
+    } else { $header.removeClass('scrolled'); }
+  });
+
+  if($(window).width() >= 768) $hero.height($heroContent.outerHeight());
+
+  $(window).resize(function () {
+    if($(window).width() >= 768) $hero.height($heroContent.outerHeight());
+  });
+
+  $('.carousel').slick({
+    dots: true,
+    speed: 350,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          speed: 250,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+});
